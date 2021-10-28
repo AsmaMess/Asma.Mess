838236e4521c6748c1b1b4256061a03e367a44a6
AsmaMess <messaoudiasma2019@gmail.com>
2021-10-28 12:54:02 +0200

    tp4

--- src/Controller/AsmaController.php
+++ src/Controller/AsmaController.php
@@ -17,4 +17,56 @@ class AsmaController extends AbstractController
             'controller_name' => 'AsmaController',
         ]);
     }
+// /**  
+//  * @Route("/voir/{id}",name="voir",requirements={"id"="\d+"})
+  
+//  */
+// public function voir($id): Response
+// {
+    
+//     return new  Response("hello page voir avec id".$id);
+// }
+
+/**  
+ * @Route("/voir",name="voir")
+  
+ */
+public function voir(): Response
+{
+    
+    return $this->render('asma/voir.html.twig');
+}
+public function menu(): Response
+{
+  $mymenu= array(
+['route' => 'asma' , 'intitule' => 'Accueil'],
+['route' => 'voir' , 'intitule' => 'voir un job']);
+// ['route' => 'list' , 'intitule' => 'Afficher tous les jobs']);
+
+return $this->render('asma/menu.html.twig',[
+    'mymenu'  =>$mymenu
+]);  
+}
+
+
+public function sidebar(): Response
+{
+  $listjobs= array(
+['id' => 1, 'nomjob' => 'Developpeur web'],
+['id' => 2, 'nomjob' => 'Responsable marketing'],
+['id' => 3 , 'nomjob' => 'Team Leader']);
+
+
+return $this->render('asma/sidebar.html.twig',[
+    'listjobs'  =>$listjobs
+]);  
+}
+
+
+
+
+
+
+
+
 }
